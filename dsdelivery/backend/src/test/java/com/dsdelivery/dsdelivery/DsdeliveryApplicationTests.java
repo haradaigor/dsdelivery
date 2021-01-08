5e82ab8b44713f1f1d279e9f73e57ddd3135d86f
unknown <haradaabreu37@gmail.com>
2021-01-08 15:07:15 -0300

    Project created

---
+++ dsdelivery/backend/src/test/java/com/dsdelivery/dsdelivery/DsdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsdelivery.dsdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
