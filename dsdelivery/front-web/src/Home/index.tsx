2ada367aa74bf973bc750af75e9b1f981311a326
unknown <haradaabreu37@gmail.com>
2021-02-02 21:44:00 -0300

    Create project base

---
+++ dsdelivery/front-web/src/Home/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './styles.css';
+import {ReactComponent as MainImage } from './main.svg';
+
+function Home (){
+    return(
+        <div className="home-container">
+            <div className="home-content">
+                <div className="home-actions">
+                    <h1 className="home-title">
+                        Make your order <br/> that we delivery <br/> for you!!!
+                    </h1>
+                    <h3 className="home-subtitle">
+                        Choose your order and in a few minutes <br/>
+                        we take to you
+                    </h3>
+                    <a href="orders" className="home-btn-order">
+                        MAKE ORDER
+                    </a>
+                </div>
+                <div className="home-image">
+                    <MainImage/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
