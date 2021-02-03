7d4215a053b0bec535ee0dc4fe90db7ac2ffeef4
unknown <haradaabreu37@gmail.com>
2021-02-03 00:17:55 -0300

    Add API request and build content with back-end data

--- dsdelivery/front-web/src/Orders/index.tsx
+++ dsdelivery/front-web/src/Orders/index.tsx
@@ -1,11 +1,24 @@
 import './styles.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import StepsHeader from './StepsHeadr';
+import ProductsList from './ProductsList';
+import { Product } from './Types';
+import { fetchProducts } from '../api';
 
 function Orders (){
+    const [products, setProducts] = useState<Product[]>([]);
+
+    console.log(products);
+    useEffect(()=>{
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
     return(
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products} />
         </div>
     );
 }
